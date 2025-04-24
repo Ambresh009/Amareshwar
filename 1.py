1073661cafaf07ed87a8ed7fca7020711e855595
Ambresh009 <96663988+Ambresh009@users.noreply.github.com>
2025-04-24 12:46:21 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+mystring="pack up my box with five dozen liqour jugs"
+#slicing operator [a:b:c]
+print(mystring[::])
+print(mystring[:])
+print(mystring[5:])
+print(mystring[8:12])
+print(mystring[11:35:2])
+print(len(mystring))
+print(mystring[-20:-10])
+print(mystring[::-1])
+print(mystring[-10:-20:-1])
